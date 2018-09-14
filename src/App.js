4b408268c24efe24e10926dac713d5ac2208240a
Danielle Grandison <xani@Danielles-MBP-3.fios-router.home>
2018-09-13 20:09:18 -0400

    Dani said no

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Background from './images/background.gif';
+import './App.css';
+
+var sectionStyle = {
+  width: "100%",
+  height: "100%",
+  backgroundImage: `url(${Background})`
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>hey </p>
+        
+        
+        
+      </div>
+    );
+  }
+}
+
+export default App;
